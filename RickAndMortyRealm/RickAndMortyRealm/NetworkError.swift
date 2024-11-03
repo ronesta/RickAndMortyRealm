48fcd83874840800d9e52d60af01f10e6338d736
ronesta <gvozzzd123@gmail.com>
2024-11-03 20:08:51 +0300

    Create and setup NetworkManager

---
+++ RickAndMortyRealm/RickAndMortyRealm/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  RickAndMortyRealm
+//
+//  Created by Ибрагим Габибли on 03.11.2024.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+}
